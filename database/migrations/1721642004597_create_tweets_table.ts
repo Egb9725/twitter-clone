795b8ca797e637214ccb219fc7e10290e11f3a5f
Dorcas Egbende <dorcasegbende@gmail.com>
2024-07-22 12:09:48 +0200

    create models

--- database/migrations/1721642004597_create_tweets_table.ts
+++ database/migrations/1721642004597_create_tweets_table.ts
@@ -1,14 +1,13 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'users'
+  protected tableName = 'tweets'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('email').unique().notNullable()
-      table.string('password').notNullable()
-      table.string('username').unique().notNullable()
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.text('content').notNullable()
       table.timestamps(true, true)
     })
   }
